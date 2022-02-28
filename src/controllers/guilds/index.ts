17cf6152200b8f6d63f02bbdf5cdbe0222ab9c67
ManassesM <manamiui2001@gmail.com>
2022-02-28 11:55:39 -0300

    getMutualGuildsService

--- src/controllers/guilds/index.ts
+++ src/controllers/guilds/index.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express'
 import { User } from '../../database/schemas/User'
-import { getBotGuildservice, getUserGuildsService } from '../../services/guilds'
+import {
+	getBotGuildservice,
+	getMutualGuildsService,
+	getUserGuildsService,
+} from '../../services/guilds'
 
 export async function getGuildsController(req: Request, res: Response) {
 	const user = req.user as User
@@ -8,17 +12,13 @@ export async function getGuildsController(req: Request, res: Response) {
 	try {
 		
     // fetching data
-    const { data: botGuilds } = await getBotGuildservice()
-		const { data: userGuilds } = await getUserGuildsService(user.id)
-		
-    // response
-    res.send({
-			botGuilds,
-			userGuilds,
-		})
+		const guilds = await getMutualGuildsService(user.id)
+
+		// response
+		res.send({ guilds })
 
 	} catch (err) {
-    console.log(err)
+		console.log(err)
 		res.status(400).send('Error')
 	}
 }
--- src/services/guilds/index.ts
+++ src/services/guilds/index.ts
@@ -2,11 +2,10 @@ import { api } from '../../utils/contants'
 import { User } from '../../database/schemas'
 import { PartialGuild } from '../../utils/types'
 
-
 // -- get all the guilds which the bot is connected
 export function getBotGuildservice() {
-  const TOKEN = process.env.DISCORD_BOT_TOKEN
-  
+	const TOKEN = process.env.DISCORD_BOT_TOKEN
+
 	return api.get<PartialGuild[]>(`/users/@me/guilds`, {
 		headers: { Authorization: `Bot ${TOKEN}` },
 	})
@@ -16,7 +15,24 @@ export function getBotGuildservice() {
 export async function getUserGuildsService(id: string) {
 	const user = await User.findById(id)
 	if (!user) throw new Error('No user found')
+
 	return api.get<PartialGuild[]>(`/users/@me/guilds`, {
 		headers: { Authorization: `Bearer ${user.accessToken}` },
 	})
+}
+
+// -- get all the guilds where the user is an admin and both bot and user are members 
+export async function getMutualGuildsService(id: string) {
+	const { data: botGuilds } = await getBotGuildservice()
+	const { data: userGuilds } = await getUserGuildsService(id)
+
+	// get all the guilds where the user is an admin
+	const adminUserGuilds = userGuilds.filter(
+		({ permissions }) => (parseInt(permissions) & 0x8) === 0x8
+	)
+  
+  // return the guilds where the bot and the user are members 
+	return adminUserGuilds.filter((guild) =>
+		botGuilds.some((botGuild) => botGuild.id === guild.id)
+	)
 }
